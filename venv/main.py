64d11bf6a4940c6bc6a0ada5683b184c38715d0a
Sean <sean.fletcher17@gmail.com>
2020-05-05 20:51:25 -0400

    Fixed formatting

--- venv/main.py
+++ venv/main.py
@@ -4,20 +4,16 @@ from summarize import summarizetext
 from summarize import nltkdownload
 
 r = tk.Tk()
-summaryLabel = Label(r, text = "If this is your first time running this program, please his the download NLTK Data button.", width = 100, wraplength = 400, justify = "left")
+summaryLabel = Label(r, text = "If this is your first time running this program, please hit the download NLTK Data button.", width = 100, wraplength = 400, justify = "left")
 def summarizebuttonaction():
     summaryText = summarizetext(inputUrl.get())
     summaryLabel.config(text = summaryText)
-
 
 r.title("Summarize Website")
 inputUrl = tk.StringVar()
 urlEntry = Entry(r, width=50, textvariable = inputUrl)
 summarizeButton = Button(r, text='Summarize', width=25, command = lambda: summarizebuttonaction())
 nltkDataButton = Button(r, text = "Download NLTK Data", command = lambda: nltkdownload())
-
-
-
 
 urlEntry.pack()
 summarizeButton.pack()
